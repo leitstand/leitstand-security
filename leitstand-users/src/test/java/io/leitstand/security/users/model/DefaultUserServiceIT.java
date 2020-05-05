c24ac2adbd167c91aee697960f7245ee3cc78ea1
Martin Steger <martin@rtbrick.com>
2020-05-05 19:28:22 +0200

    Report password mismatch properly

--- leitstand-users/src/test/java/io/leitstand/security/users/model/DefaultUserServiceIT.java
+++ leitstand-users/src/test/java/io/leitstand/security/users/model/DefaultUserServiceIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 RtBrick Inc.
+4 * Copyright 2020 RtBrick Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
@@ -35,10 +35,13 @@ import static org.mockito.Mockito.when;
 import javax.security.enterprise.credential.Password;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.ArgumentCaptor;
 
 import io.leitstand.commons.EntityNotFoundException;
+import io.leitstand.commons.UnprocessableEntityException;
 import io.leitstand.commons.db.DatabaseService;
 import io.leitstand.commons.messages.Message;
 import io.leitstand.commons.messages.Messages;
@@ -48,9 +51,12 @@ import io.leitstand.security.auth.UserId;
 import io.leitstand.security.auth.UserName;
 import io.leitstand.security.users.service.UserSettings;
 import io.leitstand.security.users.service.UserSubmission;
+import io.leitstand.testing.ut.LeitstandCoreMatchers;
 
 public class DefaultUserServiceIT extends UsersIT {
 
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 	
 	private DefaultUserService service;
 	private ArgumentCaptor<Message> message;
@@ -81,6 +87,9 @@ public class DefaultUserServiceIT extends UsersIT {
 	
 	@Test
 	public void cannot_create_user_with_invalid_password_confirmation() {
+		exception.expect(UnprocessableEntityException.class);
+		exception.expect(LeitstandCoreMatchers.reason(IDM0008E_PASSWORD_MISMATCH));
+		
 		UserSubmission user = newUserSubmission()
 							  .withUserName(UserName.valueOf("wrong_confirm"))
 							  .withPassword(new Password("unittest"))
@@ -90,7 +99,6 @@ public class DefaultUserServiceIT extends UsersIT {
 		transaction(()->{
 			service.addUser(user);
 		});
-		assertEquals(IDM0008E_PASSWORD_MISMATCH.getReasonCode(),message.getValue().getReason());
 		
 	}
 	
