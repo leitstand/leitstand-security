e47e64c6981f3ed396ed735efecf84cbe2070eea
Martin Steger <martin@rtbrick.com>
2022-10-02 21:53:51 +0200

    Enable basic authentication when RBMS built-in user repository is used

--- leitstand-auth-impl/src/main/java/io/leitstand/security/auth/basic/BasicAuthConfig.java
+++ leitstand-auth-impl/src/main/java/io/leitstand/security/auth/basic/BasicAuthConfig.java
@@ -4,6 +4,9 @@ import static io.leitstand.commons.etc.Environment.getSystemProperty;
 import static java.lang.Boolean.parseBoolean;
 
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import io.leitstand.security.sso.oidc.config.OidcConfig;
 
 /**
  * HTTP Basic Authentication configuration.
@@ -14,13 +17,20 @@ class BasicAuthConfig {
     private static final boolean BASIC_AUTH_ENABLED = parseBoolean(getSystemProperty("BASIC_AUTH_ENABLED","false"));
 
 
+    @Inject
+    private OidcConfig oidcConfig;
+    
     
     /**
      * Returns whether HTTP Basic Authentication support is enabled or not.
      * @return <code>true</code> if HTTP Basic Authentication support is enabled, <code>false</code> otherwise.
      */
     public boolean isBasicAuthEnabled() {
-        return BASIC_AUTH_ENABLED;
+        // In case no OpenID/Connect server is configured,
+    	// RBMS becomes the authorization server to spawn a SSO domain over RBMS and grafana.
+    	// Grafana uses basic authentication - as specified by Oauth - to authenticate the call 
+    	// for an access token which is part of the OAuth authorization flow.
+    	return BASIC_AUTH_ENABLED || oidcConfig == null;
     }
     
     
